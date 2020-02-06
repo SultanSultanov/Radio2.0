bd69b8ee27c0c4d73439416e6ec509573f2d4cc2
Sultanov <sull2007@list.ru>
2020-02-06 13:32:56 +0300

    1

--- src/main/java/Simple.java
+++ src/main/java/Simple.java
@@ -6,8 +6,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 import javax.swing.*;
 
 
@@ -17,37 +19,47 @@ public class Simple extends JFrame {
     JPanel panel;
     Player currentPlayer;
     Font font = new Font("TimesNewRoman", Font.BOLD, 22);
+    Properties property = new Properties();
+    FileInputStream fis;
+
 
 
     public Simple() {
         setLocationByPlatform(true);
         setBounds(700, 300, 300, 600);
         JPanel panel = new JPanel();
-
         panel.setLayout(new GridLayout(6, 1));
 
-        JButton button1 = new JButton("Radio SPB");
-        button1.setActionCommand("https://myradio24.org/2666");
+        try {
+            fis = new FileInputStream("src/main/resources/radio.properties");
+            property.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JButton button1 = new JButton(property.getProperty("button1.name"));
+        button1.setActionCommand(property.getProperty("button1.URL"));
         button1.setFont(font);
 
-        JButton button2 = new JButton("Retro FM");
-        button2.setActionCommand("https://myradio24.org/8144");
+
+        JButton button2 = new JButton(property.getProperty("button2.name"));
+        button2.setActionCommand(property.getProperty("button2.URL"));
         button2.setFont(font);
 
-        JButton button3 = new JButton("Great radio");
-        button3.setActionCommand("https://myradio24.org/60403");
+        JButton button3 = new JButton(property.getProperty("button3.name"));
+        button3.setActionCommand(property.getProperty("button3.URL"));
         button3.setFont(font);
 
-        JButton button4 = new JButton("Radio Respect");
-        button4.setActionCommand("https://myradio24.org/lucu666");
+        JButton button4 = new JButton(property.getProperty("button4.name"));
+        button4.setActionCommand(property.getProperty("button4.URL"));
         button4.setFont(font);
 
-        JButton button5 = new JButton("Radio-J");
-        button5.setActionCommand("https://myradio24.org/evgewa");
+        JButton button5 = new JButton(property.getProperty("button5.name"));
+        button5.setActionCommand(property.getProperty("button5.URL"));
         button5.setFont(font);
 
-        JButton button6 = new JButton("Radio Galactic");
-        button6.setActionCommand("https://myradio24.org/11643");
+        JButton button6 = new JButton(property.getProperty("button6.name"));
+        button6.setActionCommand(property.getProperty("button6.URL"));
         button6.setFont(font);
 
         panel.add(button1);
@@ -77,8 +89,7 @@ public class Simple extends JFrame {
             }
             new Thread(() -> {
                 try {
-                    Player player = new Player(new BufferedInputStream(new URL(e.getActionCommand()).openStream()));
-                    currentPlayer = player;
+                    currentPlayer = new Player(new BufferedInputStream(new URL(e.getActionCommand()).openStream()));
                     currentPlayer.play();
                 } catch (JavaLayerException | IOException ex) {
                     ex.printStackTrace();
